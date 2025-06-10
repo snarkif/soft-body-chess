70ca09255ee0dd040aa94f8626fb87cc0fbd03f7
snark <snirkassif2169@gmail.com>
2025-06-10 17:16:15 +0300

    Update start.js

--- docs/start.js
+++ docs/start.js
@@ -4,21 +4,21 @@ let angle = 0;
 
 ////////////////////////
 function start(arr,n,startTimer){
-  
+
   if(mouseIsPressed&& mouseX>width-70&&mouseX<width-20&&mouseY>100&&mouseY<150&&f==true){
         return false;
-    
-      }
-  
 
-  
-  
+      }
+
+
+
+
 
 
 
   background('#077A7D');
-  
-  
+
+
   for(let b of boids){
      b.applyForce(b.avoidAll())
      b.applyForce(b.align(boids).mult(3))
@@ -30,20 +30,22 @@ function start(arr,n,startTimer){
     }
      b.update()
   }
-  
+
   for(let b of boids){
   b.show()
   }
  fill(30, 30, 3); 
   rect(width-80, 110, 50, 50, 5);
-  
+
   fill(245, 196, 94);
   rect(width-70,100,50,50,5);
   textSize(40)
   textFont("Courier New")
   text("softbody chess",400,550)
+  textSize(20)
+  text("by koren, uri and snir ",403,580)
   return true;
 
-  
+
 }
 ///////////////////////////
